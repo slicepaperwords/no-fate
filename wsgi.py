98f98ab5875fe653d98ef80e7ac0bee0a4243fdd
slicepaperwords <chris@shout.net>
2018-12-05 00:25:02 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,36 +5,14 @@ application = Flask(__name__)
 def hello_world():
     print("BooYa x 3!!")
 
-def hello():
-    print("hello yo")
-
-def totes():
-    print("this is totes.")
-
 hello_world()
-hello()
-totes()
 hello_world()
-hello()
-totes()
 hello_world()
-hello()
-totes()
 hello_world()
-hello()
-totes()
 hello_world()
-hello()
-totes()
 hello_world()
-hello()
-totes()
 hello_world()
-hello()
-totes()
 hello_world()
-hello()
-totes()
 
 if __name__ == "__main__":
     application.run()
